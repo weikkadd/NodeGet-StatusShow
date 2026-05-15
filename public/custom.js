841bf7ba0efc4d415a637150403c6821e8009806
weimei <weimei12@zohomail.cn>
2026-05-15 17:01:07 +0800

    Update backend URL for SITE_1 configuration

--- public/custom.js
+++ public/custom.js
@@ -1,5 +1,5 @@
 //SITE_NAME=我的探针
 SITE_LOGO=https://example.com/logo.png
 SITE_FOOTER=Powered by NodeGet
-SITE_1=name="master-1",backend_url="wss://ellis-attention-hospitality-while.trycloudflare.com",token="7R0omn3X7ADKMmYY:5dli7PosK8sT5wDfSgdR7QcjD6jbZ3dp"
+SITE_1=name="master-1",backend_url="wss://while.trycloudflare.com",token="7R0omn3X7ADKMmYY:5dli7PosK8sT5wDfSgdR7QcjD6jbZ3dp"
 SITE_2=name="master-2",backend_url="wss://m2.example.com",token="xyz789" 
